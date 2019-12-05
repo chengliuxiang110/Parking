55ff655c1ab3bd1e53c756eb7ed4933bd7acd747
lenovo <lenovo@LAPTOP-GBRR5U31>
2019-12-05 14:31:53 +0800

    1

--- ParKingMVC/Controllers/UserInfoController.cs
+++ ParKingMVC/Controllers/UserInfoController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,40 @@ namespace ParKingMVC.Controllers
         {
             return View();
         }
+
+        #region 用户登录、注册
+
+
+        #region 登录
+        public ActionResult UserInfoLogon()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void UserInfoLogon(string Name,string Key)
+        {
+            UserInfoModel m = new UserInfoModel() { Uname = Name, Upwd = Key };
+            url+= "Login/Login";
+            string s = JsonConvert.SerializeObject(m);
+            string data = HttpClientHeper.Post(url, s);
+            List<UserInfoModel> list = JsonConvert.DeserializeObject<List<UserInfoModel>>(data);
+            if (list.Count > 0)
+            {
+                Response.Write("<script>alert('登录成功！');location.href='/Users/Show'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('登录失败！');location.href='/Users/Show'</script>");
+            }
+        }
+
+        #endregion
+        #region 注册
+
+        #endregion
+        #endregion
+
+
         /// <summary>
         /// 个人信息反填
         /// </summary>
