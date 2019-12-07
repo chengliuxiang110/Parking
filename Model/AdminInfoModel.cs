cc547bb65125faeddb8a208162a8f3f75be84008
伟伟 张 <2710867153@qq.com>
2019-12-07 08:14:20 +0800

    M

---
+++ Model/AdminInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMvc.Model
+{
+    public class AdminInfoModel
+    {
+        //管理员类
+        public int Sida { get; set; }
+        public string Sname { get; set; }
+        public string Keyword { get; set; }
+    }
+}
---
+++ Model/CarTypesInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMvc.Model
+{
+    public class CarTypesInfoModel
+    {
+        //车辆类别
+        public int TID { get; set; }
+        public string Tname { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
