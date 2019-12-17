dcf6c5cadba3a965d4125c6b1f43502a3b983b15
伟伟 张 <2710867153@qq.com>
2019-12-17 20:25:35 +0800

    移动

---
+++ Applet/Controllers/EnterongController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Applet.Controllers
+{
+    public class EnterongController : Controller
+    {
+        // GET: Enterong
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+        public ActionResult Make()
+        {
+            return View();
+        }
+
+    }
+}
